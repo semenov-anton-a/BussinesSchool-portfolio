bbb7c60469f17d61362c68f65cd4925494840a43
Anton Semenov <semenov.anton.a@gmail.com>
2021-08-19 16:44:56 +0300

    small FIXs

--- assests/js/main.js
+++ assests/js/main.js
@@ -1,38 +1,27 @@
 $(document).ready(function () {
 
     function counter( obj ) {
-        $(obj).prop('Counter',0).animate({Counter: $(obj).text()}, 
-        {
-            duration: 1500,
-            easing: 'swing',
-            step: function (now) {
-                $(obj).text(Math.ceil(now)+"%");
+        $(obj).prop('Counter',0).animate({ Counter: $(obj).text() },
+            {
+                duration: 3800, easing: 'swing',
+                step: function (now) { $(obj).text(Math.ceil(now)+"%") }
             }
-        });
+        );
     }
 
     let $myProgress = $("#myProgress");
 
-    let $procHTML = $("#multiCollapseExample1 ul li:nth-child(1)");
-
-    const awaitTimeout = delay => new Promise(resolve => setTimeout(resolve, delay));
-
-    PromiseProgressBar = function ( $bar ) {
-        awaitTimeout(300).then( () => {
-                let proc = $bar.find('span.count').text();
-                counter( $bar.find('span.count') )
-                $bar.animate({ width: proc+"%" }, 1000)
-            });
+    ProgressBar = function ( $bar ) {
+        let proc = $bar.find('span.count').text();
+        counter( $bar.find('span.count') )
+        $bar.animate({ width: proc+"%" }, 3000)
         return false;
     };
-    
-    
-
 
     $('#myProgress button').click(function(){
         $($myProgress).find('li').each(function(index){
             let $this = $(this);
-            PromiseProgressBar( $this.find('div.progress-bar') );
+            ProgressBar( $this.find('div.progress-bar') );
         });
     })
 
